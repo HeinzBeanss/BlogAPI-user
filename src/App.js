693ad9d4e86f7bd84cc86f0627c5a63ace1a9bb9
David Bean <heinzbeansss@gmail.com>
2023-05-07 03:56:42 +0100

    Slight change in paragraph text

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
         <div className='left-side'>
           <h2 className='welcome'>Welcome to the journals.</h2>
           <p className='welcomepara'>Welcome to my blog, where I document and note everything from the most mundane matters, to the most exciting and vital thoughts that go on inside my head. </p>
-          <p>Feel free to share your thoughts and opinions on their respective articles, I look forward to hearing what you have to say!</p>
+          <p>Feel free to share your thoughts and opinions on their respective articles, I look forward to hearing what you have to say! Also, if there are no posts then I have refused to pay for the back-end to be maintained, so the fecth calls aren't working, forgive me for being so cheap!</p>
         </div>
         <div className='post-grid'>
         {data.map(post => 
